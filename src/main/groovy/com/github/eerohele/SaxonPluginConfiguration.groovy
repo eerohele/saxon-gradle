831c9e74cfbcf5fde3c63ce159dd6bb4e38ab94e
Norman Walsh <ndw@nwalsh.com>
2021-05-08 17:09:34 +0100

    Rework global configuration; support classpath dynamically

--- src/main/groovy/com/github/eerohele/SaxonPluginConfiguration.groovy
+++ src/main/groovy/com/github/eerohele/SaxonPluginConfiguration.groovy
@@ -1,5 +1,22 @@
 package com.github.eerohele
 
 @Singleton
-class SaxonPluginConfiguration implements OptionsConfig {
+class SaxonPluginConfiguration implements SaxonPluginOptions {
+    // A singleton that holds the global configuration options.
+
+    final Map<String, Object> options = [:]
+    final Map<String, String> advancedOptions = [:]
+    final Map<String, String> pluginOptions = [:]
+
+    void setOption(String name, Object value) {
+        options[name] = value
+    }
+
+    void setAdvancedOption(String name, Object value) {
+        advancedOptions[name] = value
+    }
+
+    void setPluginOption(String name, Object value) {
+        pluginOptions[name] = value
+    }
 }
--- src/main/groovy/com/github/eerohele/SaxonXsltTask.groovy
+++ src/main/groovy/com/github/eerohele/SaxonXsltTask.groovy
@@ -1,9 +1,16 @@
 package com.github.eerohele
 
-import groovy.util.slurpersupport.GPathResult
+import org.xml.sax.InputSource
+import javax.xml.transform.sax.SAXSource
+
+import net.sf.saxon.s9api.DocumentBuilder
+import net.sf.saxon.s9api.Processor
+import net.sf.saxon.s9api.QName
+import net.sf.saxon.s9api.SaxonApiException
+import net.sf.saxon.s9api.XPathExecutable
+import net.sf.saxon.s9api.XdmNode
+
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.ConfigurableFileCollection
-import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.InputFiles
@@ -12,35 +19,32 @@ import org.gradle.api.tasks.SkipWhenEmpty
 import org.gradle.api.file.FileCollection
 import org.gradle.api.InvalidUserDataException
 
-import org.apache.xml.resolver.Catalog
-import org.apache.xml.resolver.CatalogManager
-
 import org.gradle.workers.WorkQueue
 import org.gradle.workers.WorkerExecutor
 
 import javax.inject.Inject
 import java.nio.file.Path
 
-@SuppressWarnings('MethodCount')
-class SaxonXsltTask extends DefaultTask implements OptionsConfig {
+class SaxonXsltTask extends DefaultTask implements SaxonPluginOptions {
     protected static final String PERIOD = '.'
     protected static final String XSLT_NAMESPACE = 'http://www.w3.org/1999/XSL/Transform'
+    protected static final QName METHOD = new QName('method')
+    protected static final String INPUT_OPTION = 'input'
+    protected static final String OUTPUT_OPTION = 'output'
+    protected static final String STYLESHEET_OPTION = 'stylesheet'
 
     protected final List<String> defaultArguments = ['-quit:off'].asImmutable()
 
     protected final Map<String, Object> options = [output: project.buildDir]
-
+    protected final Map<String, String> advancedOptions = [:]
     protected final Map<String, Object> pluginOptions = [outputDirectoryLayout: 'flat']
-
-    @Classpath
-    final ConfigurableFileCollection classpath
 
     protected Map<String, String> stylesheetParams = [:]
 
-    private final XmlSlurper xmlSlurper = new XmlSlurper()
-    private GPathResult xslt
-    private Catalog xmlCatalog
-    private final CatalogManager catalogManager = new CatalogManager()
+    private final Processor processor
+    private final DocumentBuilder builder
+    private final XPathExecutable findOutput
+    private XdmNode xslt = null
 
     // Saxon command-line options take 'on' and 'off', but it's best to let the
     // users use booleans as well.
@@ -61,43 +65,94 @@ class SaxonXsltTask extends DefaultTask implements OptionsConfig {
     SaxonXsltTask(WorkerExecutor workerExecutor) {
         super()
         this.workerExecutor = workerExecutor
-        this.classpath = project.objects.fileCollection()
-        xmlSlurper.setFeature('http://apache.org/xml/features/disallow-doctype-decl', false)
-        xmlSlurper.setFeature('http://apache.org/xml/features/nonvalidating/load-external-dtd', false)
-        catalogManager.setIgnoreMissingProperties(true)
+
+        processor = new Processor(false)
+        builder = processor.newDocumentBuilder()
+        builder.setDTDValidation(false)
+        builder.setLineNumbering(true)
+
+        def xpc = processor.newXPathCompiler()
+        xpc.declareNamespace("xsl", XSLT_NAMESPACE)
+        findOutput = xpc.compile("//xsl:output[not(@name)]")
     }
 
-    // START COMMON OPTIONS
+    // ============================================================
+
+    void setOption(String name, Object value) {
+        options[name] = value
+    }
+
+    void setAdvancedOption(String name, Object value) {
+        advancedOptions[name] = value
+    }
+
+    void setPluginOption(String name, Object value) {
+        pluginOptions[name] = value
+    }
+
+    Object getOption(String name) {
+        return name in options ? options[name] :
+            SaxonPluginConfiguration.instance.options[name]
+    }
+
+    Object getAdvancedOption(String name) {
+        return name in advancedOptions ? advancedOptions[name] :
+            SaxonPluginConfiguration.instance.advancedOptions[name]
+    }
+
+    Object getPluginOption(String name) {
+        return name in pluginOptions ? pluginOptions[name] :
+            SaxonPluginConfiguration.instance.pluginOptions[name]
+    }
+
+    // ============================================================
 
     void input(Object input) {
-        this.options.input = input
+        setOption(INPUT_OPTION, input)
     }
 
     void output(Object output) {
-        this.options.output = project.file(output)
+        setOption(OUTPUT_OPTION, project.file(output))
     }
 
-    @SuppressWarnings('CatchException')
     void stylesheet(Object stylesheet) {
-        this.options.stylesheet = project.file(stylesheet)
-        if (this.options.stylesheet.exists()) {
-            try {
-                this.xslt = this.xmlSlurper
-                        .parse(stylesheet)
-                        .declareNamespace(xsl: XSLT_NAMESPACE)
-            } catch (Exception ex) {
-                logger.warn("Failed to parse: ${this.options.stylesheet}")
-                logger.warn("  ${ex.getMessage()}")
+        this.xslt = null
+        try {
+            // If it's a string that looks like a URI, try to make it a URI
+            if (stylesheet instanceof String && stylesheet ==~ /^\S+:\/\/.*/) {
+                try {
+                    stylesheet = new URI(stylesheet)
+                } catch (URISyntaxException ex) {
+                    // nevermind
+                }
             }
+
+            if (stylesheet instanceof URI) {
+                // If it's a file: URI, turn it into a file
+                if (stylesheet.getScheme() == "file") {
+                    def stylefile = project.file(stylesheet.getPath())
+                    setOption(STYLESHEET_OPTION, stylefile)
+                    if (stylefile.exists()) {
+                        this.xslt = builder.build(stylefile)
+                    }
+                } else {
+                    // Otherwise just try to load it (for the output method)
+                    setOption(STYLESHEET_OPTION, stylesheet)
+                    def source = new InputSource(stylesheet.toASCIIString())
+                    this.xslt = builder.build(new SAXSource(source))
+                }
+            } else {
+                // If it's not a URI, assume it's a file, load it if it exists
+                def stylefile = project.file(stylesheet)
+                setOption(STYLESHEET_OPTION, stylefile)
+                if (stylefile.exists()) {
+                    this.xslt = builder.build(stylefile)
+                }
+            }
+        } catch (SaxonApiException ex) {
+            logger.warn("Failed to parse: ${stylesheet}")
+            logger.warn("  ${ex.getMessage()}")
         }
-    }
-
-    void outputFileExtension(String extension) {
-        this.pluginOptions.outputFileExtension = extension
-    }
-
-    void outputDirectoryLayout(String layout) {
-        this.pluginOptions.outputDirectoryLayout = layout
     }
 
     void parameters(Map<String, String> parameters) {
@@ -106,53 +161,22 @@ class SaxonXsltTask extends DefaultTask implements OptionsConfig {
 
     @Internal
     protected String getDefaultOutputExtension() {
-        if (this.pluginOptions.outputFileExtension != null) {
-            return this.pluginOptions.outputFileExtension
+        if (getPluginOption('outputFileExtension') != null) {
+            return getPluginOption('outputFileExtension')
         }
 
         // Read output file extension from the <xsl:output> element of the
-        // stylesheet.
-        String method = this.xslt.output.@method
-        return method ? method : 'xml'
-    }
-
-    protected URI resolveUri(String path) {
-        URI uri = new URI(path)
-
-        if (this.options.catalog) {
-            String resolved = xmlCatalog.resolveURI(path)
-            resolved ? new URI(resolved) : uri
-        } else {
-            uri
-        }
-    }
-
-    @SuppressWarnings('CatchException')
-    protected FileCollection getIncludedStylesheets(File stylesheet) {
-        def newCollection = []
-
-        if (stylesheet != null) {
-          newCollection += stylesheet
-
-          try {
-            GPathResult xslt = this.xmlSlurper.parse(stylesheet).declareNamespace(xsl: XSLT_NAMESPACE)
-
-            (xslt.include + xslt.import).each { elem ->
-              URI href = resolveUri(elem.@href[0].toString())
-              URI uri = stylesheet.toURI().resolve(href)
-              if (uri.getScheme() == "file") {
-                newCollection += getIncludedStylesheets(new File(uri))
-              }
+        // stylesheet. This is only a heuristic.
+        String method = null
+        if (this.xslt != null) {
+            def selector = findOutput.load()
+            selector.setContextItem(this.xslt)
+            selector.iterator().each { snode ->
+                method = snode.getAttributeValue(METHOD)
             }
-          } catch (FileNotFoundException ex) {
-            // nevermind
-          } catch (Exception ex) {
-            logger.warn("Failed to parse: ${stylesheet}")
-            logger.warn("  ${ex.getMessage()}")
-          }
-
-          return project.files(newCollection)
         }
+
+        return method ? method : 'xml'
     }
 
     String getOutputFileName(File file) {
@@ -174,9 +198,10 @@ class SaxonXsltTask extends DefaultTask implements OptionsConfig {
     protected File getOutputFile(File file) {
         Path filePath = file.toPath()
         Path rootPath = project.rootDir.toPath()
-        Path outputPath = project.file(this.options.output).toPath()
+        Path outputPath = project.file(getOption(OUTPUT_OPTION)).toPath()
 
-        if (this.pluginOptions.outputDirectoryLayout == 'nested' && filePath.startsWith(rootPath) && outputPath.startsWith(rootPath)) {
+        if (getPluginOption('outputDirectoryLayout') == 'nested'
+            && filePath.startsWith(rootPath) && outputPath.startsWith(rootPath)) {
             Path dir = rootPath.relativize(filePath).parent
             outputPath.resolve(dir).resolve(getOutputFileName(file)).toFile()
 
@@ -193,32 +218,34 @@ class SaxonXsltTask extends DefaultTask implements OptionsConfig {
             //
             // Anyway, this approach likely covers a large chunk of how people
             // would conceivably use this feature anyway.
-        } else if (project.files(this.options.input).size() == 1 && project.buildDir.getAbsolutePath() != this.options.output.getAbsolutePath()) {
-            this.options.output
+        } else if (project.files(getOption(INPUT_OPTION)).size() == 1
+                   && project.buildDir.getAbsolutePath() != getOption(OUTPUT_OPTION).getAbsolutePath()) {
+            getOption(OUTPUT_OPTION)
         } else {
-            new File(this.options.output, getOutputFileName(file))
+            new File(getOption(OUTPUT_OPTION), getOutputFileName(file))
         }
     }
 
     @OutputFiles
     FileCollection getOutputFiles() {
-        if (this.options.input != null) {
-            project.files(project.files(this.options.input).collect { getOutputFile(it) })
+        if (getOption(INPUT_OPTION) != null) {
+            project.files(project.files(getOption(INPUT_OPTION)).collect { getOutputFile(it) })
         } else {
-            project.files(this.options.output)
+            project.files(getOption(OUTPUT_OPTION))
         }
     }
 
     @InputFiles
     @SkipWhenEmpty
     FileCollection getInputFiles() {
-        FileCollection stylesheets = project.files() + getIncludedStylesheets(this.options.stylesheet)
-
-        if (this.options.input != null) {
-            project.files(this.options.input) + stylesheets
-        } else {
-            stylesheets
+        FileCollection files = project.files()
+        if (getOption(INPUT_OPTION) != null) {
+            files += project.files(getOption(INPUT_OPTION))
         }
+        if (getOption(STYLESHEET_OPTION) != null && getOption(STYLESHEET_OPTION) instanceof File) {
+            files += project.files(getOption(STYLESHEET_OPTION))
+        }
+        return files
     }
 
     // Turn a key-value pair into a Saxon command line argument.
@@ -258,21 +285,19 @@ class SaxonXsltTask extends DefaultTask implements OptionsConfig {
     @Internal
     protected List<String> getCommonArguments() {
         Map<String, String> commonOptions = [:]
-        SaxonPluginConfiguration.instance.configuredOptions().findAll { name, value ->
+        SaxonPluginConfiguration.instance.options.findAll { name, value ->
             commonOptions[name] = value
         }
-        this.configuredOptions().findAll { name, value ->
-            commonOptions[name] = value
-        }
-        if (this.options.stylesheet) {
-            commonOptions["stylesheet"] = this.options.stylesheet
+        this.options.findAll { name, value ->
+            if (name != INPUT_OPTION && name != OUTPUT_OPTION) {
+                commonOptions[name] = value
+            }
         }
 
-        Map<String, String> advancedOptions = [:]
-        SaxonPluginConfiguration.instance.configuredAdvancedOptions().findAll { name, value ->
+        SaxonPluginConfiguration.instance.advancedOptions.findAll { name, value ->
             advancedOptions[name] = value
         }
-        this.configuredAdvancedOptions().findAll { name, value ->
+        this.advancedOptions.findAll { name, value ->
             advancedOptions[name] = value
         }
 
@@ -301,13 +326,15 @@ class SaxonXsltTask extends DefaultTask implements OptionsConfig {
     @TaskAction
     void run() {
         WorkQueue workQueue = workerExecutor.classLoaderIsolation() {
-            it.getClasspath().from(this.classpath);
+            if (getPluginOption('classpath') != null) {
+                it.getClasspath().from(getPluginOption('classpath'))
+            }
         };
 
         List<String> commonArguments = getCommonArguments() + getStylesheetParameters()
 
-        if (this.options.input != null) {
-            project.files(this.options.input).each {
+        if (getOption(INPUT_OPTION) != null) {
+            project.files(getOption(INPUT_OPTION)).each {
                 List<String> arguments = getFileSpecificArguments(it) + commonArguments
 
                 workQueue.submit(XsltTransformation) {
@@ -315,7 +342,7 @@ class SaxonXsltTask extends DefaultTask implements OptionsConfig {
                 }
             }
         } else {
-            String output = this.options.output.getPath()
+            String output = getOption(OUTPUT_OPTION).getPath()
             List<String> arguments = commonArguments + [makeSingleHyphenArgument(Argument.MAPPING.output, output)]
 
             workQueue.submit(XsltTransformation) {
